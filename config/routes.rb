8540a68d0e8f86cf6f71e3ff9b9f8e1f279fecf6
Sekori1 <romaric55220@gmail.com>
2020-01-08 14:18:42 +0100

    Themes list page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  resources :themes
   resources :authors
   resources :arts
-  root 'arts#index'
+  root to: 'themes#index'
+  
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
