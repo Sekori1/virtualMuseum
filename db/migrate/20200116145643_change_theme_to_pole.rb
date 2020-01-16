a34e977ea4d60beb83e8034be33cd13e34071495
Sekori1 <romaric55220@gmail.com>
2020-01-16 15:58:33 +0100

    edit

---
+++ db/migrate/20200116145643_change_theme_to_pole.rb
@@ -0,0 +1,4 @@
+class ChangeThemeToPole < ActiveRecord::Migration[5.2]
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_14_200646) do
+ActiveRecord::Schema.define(version: 2020_01_16_145643) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
